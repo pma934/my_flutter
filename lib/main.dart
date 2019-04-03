94cd4ef8afddf3cd1c3ae95644c52abdf6082178
Zhou Xuanzhuo <358409584@qq.com>
2019-04-03 14:29:54 +0800

    2.快速起步

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
 void main() => runApp(App());
 
@@ -7,17 +8,35 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
+        home: Home(), theme: ThemeData(primaryColor: Colors.yellow));
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+        color: Colors.white,
+        margin: EdgeInsets.all(8.0),
+        child: Column(children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(posts[index].title, style: Theme.of(context).textTheme.title),
+          Text(posts[index].author, style: Theme.of(context).textTheme.subhead)
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
         appBar: AppBar(
-          title:Text('zxz'),
+          title: Text('zxz'),
           elevation: 5.0,
-          ),
-        body:Hello()
-      ),
-      theme:ThemeData(
-        primaryColor: Colors.yellow
-      )
-    );
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        ));
   }
 }
 

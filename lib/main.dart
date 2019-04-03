074b8b609b69410858ee54746560fa82b552d9a5
Zhou Xuanzhuo <358409584@qq.com>
2019-04-03 15:54:29 +0800

    工具栏图标

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,43 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+import 'demo/listview_demo.dart';
+import 'demo/hello_demo.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-        home: Home(), theme: ThemeData(primaryColor: Colors.yellow));
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        theme: ThemeData(primaryColor: Colors.yellow));
   }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-        color: Colors.white,
-        margin: EdgeInsets.all(8.0),
-        child: Column(children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(posts[index].title, style: Theme.of(context).textTheme.title),
-          Text(posts[index].author, style: Theme.of(context).textTheme.subhead)
-        ]));
-  }
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          title: Text('zxz'),
-          elevation: 5.0,
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        leading:IconButton(
+          icon:Icon(Icons.menu),
+          tooltip:'导航',
+          onPressed: ()=>debugPrint('按下了导航按钮'),
         ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ));
-  }
-}
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Text('hello',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          )),
+        title: Text('zxz'),
+        actions:<Widget>[
+          IconButton(
+            icon:Icon(Icons.search),
+            tooltip:'搜索',
+            onPressed: ()=>debugPrint('按下了搜索按钮'),
+          ),
+        ],
+        elevation: 5.0,
+      ),
+      body: null,
     );
   }
 }
+
+

b27280e7f9b9af2008b6720891e9210a4aaac6c0
Zhou Xuanzhuo <358409584@qq.com>
2019-04-17 14:53:36 +0800

    状态管理，没有inheritedWidget

--- lib/demo/state/state_management_demo.dart
+++ lib/demo/state/state_management_demo.dart
@@ -1,37 +1,19 @@
 import 'package:flutter/material.dart';
 
-// class StateManagementDemo extends StatelessWidget {
-//   int count = 0;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('StateManagementDemo'),
-//         elevation: 0.0,
-//       ),
-//       body: Center(
-//         child: Chip(
-//           label: Text('$count'),
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         child: Icon(Icons.add),
-//         onPressed: () {
-//           count += 1;
-//           print(count);
-//         },
-//       ),
-//     );
-//   }
-// }
-
 class StateManagementDemo extends StatefulWidget {
   @override
   _StateManagementDemoState createState() => _StateManagementDemoState();
 }
 
 class _StateManagementDemoState extends State<StateManagementDemo> {
-  int count = 0;
+  int _count = 0;
+  void _increaseCount() {
+    setState(() {
+      _count += 1;
+    });
+    print(_count);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,20 +21,56 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
         title: Text('StateManagementDemo'),
         elevation: 0.0,
       ),
-      body: Center(
-        child: Chip(
-          label: Text('$count'),
-        ),
-      ),
+      body: CounterWrapper(_count, _increaseCount),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            count += 1;
-          });
-          print(count);
-        },
+        onPressed: _increaseCount,
       ),
     );
+  }
+}
+
+class CounterWrapper extends StatelessWidget {
+  final int count; //从父辈获取状态
+  final VoidCallback increaseCount; //从父辈获取回调
+  CounterWrapper(this.count, this.increaseCount);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Counter(count, increaseCount),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  final int count; //从父辈获取状态
+  final VoidCallback increaseCount; //从父辈获取回调
+  Counter(this.count, this.increaseCount);
+  @override
+  Widget build(BuildContext context) {
+    return ActionChip(
+      label: Text('$count'),
+      onPressed: increaseCount,
+    );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+
+  CounterProvider({
+    this.count,
+    this.increaseCount,
+    this.child,
+  }) : super(child: child);
+
+  static CounterProvider of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(CounterProvider);
+
+  @override
+  bool updateShouldNotify(CounterProvider oldWidget) {
+    return true;
   }
 }
